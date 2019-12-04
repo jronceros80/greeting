e62f200b7c29af41d28988b89365877b1af8a074
jronceros80 <jronceros80@gmail.com>
2019-12-04 21:53:29 +0100

    ok

--- src/main/java/net/microservice/greeting/Application.java
+++ src/main/java/net/microservice/greeting/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration	
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
